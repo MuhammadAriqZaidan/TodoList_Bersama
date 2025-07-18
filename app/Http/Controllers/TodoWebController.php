87f2af80844eaed7dae08ac61907b0fefee027bb
Muhammad Ariq Zaidan <muhammadariqidn@gmail.com>
2025-07-18 10:55:01 +0800

    add DD configuration for debugging

--- app/Http/Controllers/TodoWebController.php
+++ app/Http/Controllers/TodoWebController.php
@@ -33,6 +33,9 @@ class TodoWebController extends Controller
 
     public function store(Request $request)
     {
+
+        // Tambahkan baris ini SINI
+        dd(config('cloudinary'));
         $request->validate([
             'title' => 'required|string|max:255',
             'description' => 'nullable|string',
